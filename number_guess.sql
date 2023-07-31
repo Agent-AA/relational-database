ec4156c7e2139f28e41c2938d778ebdbd4fd9aed
Alexander Ament <118925546+Agent-AA@users.noreply.github.com>
2023-07-31 16:04:04 -0400

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,74 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_game;
+--
+-- Name: number_game; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_game WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_game OWNER TO freecodecamp;
+
+\connect number_game
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    user_name character varying(22) NOT NULL,
+    games_played integer,
+    best_game integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES ('user_1690833628094', 2, 164);
+INSERT INTO public.users VALUES ('user_1690833628095', 4, 149);
+INSERT INTO public.users VALUES ('user_1690833643771', 2, 403);
+INSERT INTO public.users VALUES ('user_1690833643772', 5, 316);
+INSERT INTO public.users VALUES ('user_1690833661120', 2, 554);
+INSERT INTO public.users VALUES ('user_1690833661121', 5, 265);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
